466f864e15e2303b2af478565421386db27423d3
Christian B <chriss@multi-byte.de>
2017-03-07 23:50:13 +0100

    recheck hotsname if rtt == 0

--- ping2influx.go
+++ ping2influx.go
@@ -29,19 +29,29 @@ func init() {
     hostname = string(os.Args[1])
     pinger = fastping.NewPinger()
 
+    resolv()
+    pinger.OnRecv = write
+
+    var err error
+    influxdb, err = client.NewHTTPClient( client.HTTPConfig{
+        Addr:     host,
+        Username: username,
+        Password: password,
+    })
+
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
+
+func resolv() {
     remoteaddr, err := net.ResolveIPAddr("ip4:icmp", hostname)
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
-    pinger.OnRecv = write
     pinger.AddIPAddr(remoteaddr)
-
-    influxdb, err = client.NewHTTPClient(client.HTTPConfig{
-        Addr:     host,
-        Username: username,
-        Password: password,
-    })
 }
 
 func ping() {
@@ -52,36 +62,45 @@ func ping() {
 }
 
 func write(addr *net.IPAddr, rtt time.Duration) {
-    // Create a new point batch
-    bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-        Database:  database,
-        Precision: "s",
-    })
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    // Create a point and add to batch
-    cur_host, err := os.Hostname()
-    tags := map [string] string {
-        "host": cur_host,
-    }
+    if rtt == 0 {
 
-    fields := map [string] interface{} {
-        "rtt": float64(rtt.Nanoseconds()) / 1000000,
-        "address": addr.String(),
-        "hostname": hostname,
-    }
+        // Dont write zero-values
+        resolv()
 
-    pt, err := client.NewPoint(hostname, tags, fields, time.Now())
-    if err != nil {
-        log.Fatal(err)
-    }
-    bp.AddPoint(pt)
+    } else {
 
-    // Write the batch
-    if err := influxdb.Write(bp); err != nil {
-        log.Fatal(err)
+        // Create a new point batch
+        bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+            Database:  database,
+            Precision: "s",
+        })
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        // Create a point and add to batch
+        cur_host, err := os.Hostname()
+        tags := map [string] string {
+            "host": cur_host,
+        }
+
+        fields := map [string] interface{} {
+            "rtt": float64(rtt.Nanoseconds()) / 1000000,
+            "address": addr.String(),
+            "hostname": hostname,
+        }
+
+        pt, err := client.NewPoint(hostname, tags, fields, time.Now())
+        if err != nil {
+            log.Fatal(err)
+        }
+        bp.AddPoint(pt)
+
+        // Write the batch
+        if err := influxdb.Write(bp); err != nil {
+            log.Fatal(err)
+        }
     }
 }
 
